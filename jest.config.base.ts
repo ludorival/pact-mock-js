a093ba779c6b63b609e3af990e9365709630cff4
Ludovic Dorival <ludorival@gmail.com>
2024-10-15 13:11:09 +0200

    wip

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -2,8 +2,8 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-
-export default {
+import type { Config } from 'jest'
+const config: Config = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
   transform: {
@@ -19,4 +19,6 @@ export default {
     },
   },
   testEnvironment: 'node',
+  reporters: [['github-actions', { silent: false }], 'summary'],
 }
+export default config
